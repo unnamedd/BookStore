887e38119ac7933f7432da4690f59706aa7216c5
Thiago Holanda <unnamedd@gmail.com>
2020-02-17 11:30:02 +0100

    Initial Commit

---
+++ BookStore/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BookStore
+//
+//  Created by Thiago Holanda on 17.02.20.
+//  Copyright © 2020 unnamedd.codes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
